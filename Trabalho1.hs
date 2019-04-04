a5c71d4be4abee47b15bb3c4c70832f526075791
fenikspp <pedro.cic.unb@gmail.com>
2019-04-04 17:09:00 -0300

    Implementacao da Primeira questão (adicionarMedicamentos)

--- Trabalho1.hs
+++ Trabalho1.hs
@@ -13,6 +13,7 @@ type Mercado = [Farmacia]
 type Compra = (Preco, Nome)
 type CompraFlex = (Preco, Nome, Medicamentos)
 
+
 {-Exemplo de Medicamentos-}
 f_Medicamentos :: Medicamentos
 f_Medicamentos = [("Dipirona", 4), 
@@ -22,7 +23,18 @@ f_Medicamentos = [("Dipirona", 4),
                   ("Ritalina", 3), 
                   ("Viagra", 4)]
 
---adicionarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
+{-::Funções auxiliares::-}
+
+--Função soma tuplas
+somaTuplas :: Medicamentos -> Medicamento -> Medicamento
+somaTuplas [(a,b)] (c,d) = (a, b+d)
+
+
+-- <\Questão 01/> Função que adiciona remedios na Lista
+adicionarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
+adicionarMedicamento (n, q) fa = if (consultarMedicamento n fa == [("",0)]) then ((n, q) : fa) 
+    else [(somaTuplas (consultarMedicamento n fa) (n, q))] ++ removerMedicamento n fa
+
 
 -- <\Questão 02/> Função que remove medicamentos da lista.
 removerMedicamento :: Nome -> Medicamentos -> Medicamentos
@@ -33,6 +45,7 @@ removerMedicamento m fa =  [ (n,q) | (n,q) <- fa, n /= m ]
 consultarMedicamento :: Nome -> Medicamentos -> Medicamentos
 consultarMedicamento m fa = if (elem m(map medicamento fa)) then [(n, q) | (n, q) <- fa, n == m] else [("",0)]
                        where medicamento (n, q) = n
+
 
 --alterarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
 
